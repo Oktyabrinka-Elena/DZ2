ae6ab600656371a0d51e510a1d53cccf02b51aa8
ElenaZamyatina <elenzamyatina12@gmail.com>
2022-09-10 03:38:29 +0400

    Задача 10 done

---
+++ ex1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите число : "); //ввод числа
+int NumberResult = Convert.ToInt32(Console.ReadLine());
+int result = ((NumberResult - (NumberResult % 10)) / 10) % 10;
+Console.WriteLine($"{NumberResult} {result}");
+
